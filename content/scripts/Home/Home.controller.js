262565c5af831f3a43e99e25f4756a56157f9e56
s.gang@vikes.csuohio.edu <s.gang@vikes.csuohio.edu>
2017-08-30 18:45:14 -0700

    Created file cards for home tab, with sample data

--- content/scripts/Home/Home.controller.js
+++ content/scripts/Home/Home.controller.js
@@ -2,6 +2,52 @@
 	angular.module("App").controller("HomeController", HomeController);
 
 	function HomeController(){
-		
+
+		var vm = this;
+
+		//Function
+
+		//Models
+		vm.sampleFiles = [
+			{
+				FileName: 'sample0.txt',
+				FileSize: '1000 bytes',
+				FileExtension: '.txt',
+				FileCreationDate: 'Sql Date'
+			},
+			{
+				FileName: 'sample0.txt',
+				FileSize: '1000 bytes',
+				FileExtension: '.txt',
+				FileCreationDate: 'Sql Date'
+			},
+			{
+				FileName: 'sample0.txt',
+				FileSize: '1000 bytes',
+				FileExtension: '.txt',
+				FileCreationDate: 'Sql Date'
+			},
+			{
+				FileName: 'sample0.txt',
+				FileSize: '1000 bytes',
+				FileExtension: '.txt',
+				FileCreationDate: 'Sql Date'
+			},
+			{
+				FileName: 'sample0.txt',
+				FileSize: '1000 bytes',
+				FileExtension: '.txt',
+				FileCreationDate: 'Sql Date'
+			},
+			{
+				FileName: 'sample0.txt',
+				FileSize: '1000 bytes',
+				FileExtension: '.txt',
+				FileCreationDate: 'Sql Date'
+			}
+		];
+
 	}
+
+
 })();
